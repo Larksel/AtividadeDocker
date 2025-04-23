3818ae863575562c42c4e5c6fc13b7cab932b2c8
Larksel <lemuelgames@outlook.com>
2025-04-23 14:07:31 -0300

    :tada: Commit inicial com servidor funcionando

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/api/users', (_req, res) => {
+  res.status(200).send('Listando usuários');
+});
+
+app.get('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  res.status(200).send('Listando usuário com id: ' + userId);
+});
+
+app.post('/api/users', (req, res) => {
+  const { nome, senha } = req.body;
+  res.status(200).send(`Criando novo usuário com nome ${nome} e senha ${senha}`);
+});
+
+app.put('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  res.status(200).send(`Atualizando usuário com id ${userId}`);
+});
+
+app.delete('/api/users/:id', (req, res) => {
+  const userId = parseInt(req.params.id);
+  res.status(200).send(`Excluindo usuário com id ${userId}`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
